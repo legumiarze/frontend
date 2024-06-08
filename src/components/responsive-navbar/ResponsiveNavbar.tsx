9d42bb6a836ed9d9db8a05da1ecfcea9cdcb642e
Mateusz Jamróz <jamr.mat@gmail.com>
2024-06-08 14:04:10 +0200

    basic routing configuration with responsive navbar

---
+++ src/components/responsive-navbar/ResponsiveNavbar.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {useTheme} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const ResponsiveNavbar: React.FC = () => {
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    const toggleDrawer =
+        (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+            if (
+                event.type === 'keydown' &&
+                ((event as React.KeyboardEvent).key === 'Tab' ||
+                    (event as React.KeyboardEvent).key === 'Shift')
+            ) {
+                return;
+            }
+            setDrawerOpen(open);
+        };
+
+    const menuItems: string[] = ['AKTUALNOŚCI',
+        'ROZKŁAD JAZDY',
+        'DLA PASAŻERA',
+        'O SPÓŁCE',
+        'KONTAKT',
+        'REKLAMA',
+        'INWESTYCJE'
+    ];
+
+    const drawer = (
+        <div
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {menuItems.map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text}/>
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            <CssBaseline/>
+            <AppBar position="static">
+                <Toolbar>
+                    {isMobile && (
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={toggleDrawer(true)}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                    )}
+                    <Typography variant="h6" style={{flexGrow: 1}}>
+                        My App
+                    </Typography>
+                    {!isMobile && (
+                        <div>
+                            {menuItems.map((item) => (
+                                <Typography
+                                    variant="button"
+                                    style={{margin: '0 10px'}}
+                                    key={item}
+                                >
+                                    {item}
+                                </Typography>
+                            ))}
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+                {drawer}
+            </Drawer>
+        </div>
+    );
+};
+
+export default ResponsiveNavbar;
